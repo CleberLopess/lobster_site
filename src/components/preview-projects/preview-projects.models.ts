983a50f43a661517c7d4aac73fdece32eb6ac7d5
Cleber Lobster <cleberlopes777@gmail.com>
2024-01-23 14:00:47 -0300

    solve building

--- src/components/preview-projects/preview-projects.models.ts
+++ src/components/preview-projects/preview-projects.models.ts
@@ -1,7 +1,7 @@
 import { StaticImageData } from "next/image";
 
 export type PreviewProjectsType = {
-  image: StaticImageData;
-  title: string;
-  description: string;
+  image?: StaticImageData;
+  title?: string;
+  description?: string;
 };
