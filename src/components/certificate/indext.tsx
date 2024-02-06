9187224ef1d7a10130a7615871849f67457844a8
Cleber Lobster <cleberlopes777@gmail.com>
2024-02-06 13:02:29 -0300

    added certificate to hero code

--- src/components/certificate/indext.tsx
+++ src/components/certificate/indext.tsx
@@ -9,7 +9,7 @@ const CardCertificate = ({
   title,
 }: cardCertificateType) => {
   return (
-    <div className="flex flex-col gap-4 max-w-52 min-h-64 rounded-lg shadow-md p-4 bg-white dark:bg-dark-bg-01 dark:text-dark-font-01">
+    <div className="flex flex-col gap-4 max-w-56 min-h-64 rounded-lg shadow-md p-4 bg-white dark:bg-dark-bg-01 dark:text-dark-font-01">
       <Image src={image ? image : placeholder} alt="certificate" />
       <span className="flex text-center justify-center">{title}</span>
       <RxExternalLink
